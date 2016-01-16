11dd1fb60401eb9689118e16ca80f8419756f296
James <james@resin.io>
2016-01-16 08:18:07 +0700

    Upload qemu binaries to github release

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM debian:jessie
 
+ENV QEMU_VERSION 2.5.0-resin
+ENV QEMU_SHA256 6525da6fef4beb5b60e2526f15fc2aa734e4395440bdd23b0ef189c57bbcea4f
+
 RUN apt-get -q update \
 	&& apt-get -qqy install \
 		build-essential \
@@ -7,23 +10,14 @@ RUN apt-get -q update \
 		zlib1g-dev \
 	&& mkdir /output
 
-ENV QEMU_VERSION 2.5.0-resin
-ENV QEMU_SHA256 6525da6fef4beb5b60e2526f15fc2aa734e4395440bdd23b0ef189c57bbcea4f
-
 RUN curl -SL https://codeload.github.com/resin-io/qemu/tar.gz/v${QEMU_VERSION} -o qemu-${QEMU_VERSION}.tar.gz \
-	&& echo "$QEMU_SHA256  qemu-${QEMU_VERSION}.tar.gz" > qemu-${QEMU_VERSION}.tar.gz.sha256sum \
+	&& echo "${QEMU_SHA256}  qemu-${QEMU_VERSION}.tar.gz" > qemu-${QEMU_VERSION}.tar.gz.sha256sum \
 	&& sha256sum -c qemu-${QEMU_VERSION}.tar.gz.sha256sum \
 	&& tar -xzf qemu-${QEMU_VERSION}.tar.gz \
 	&& rm qemu-${QEMU_VERSION}.tar.gz*
 
 WORKDIR /qemu-${QEMU_VERSION}
 
-COPY qemu-${QEMU_VERSION}.patch /qemu-${QEMU_VERSION}/qemu-${QEMU_VERSION}.patch
+COPY . /qemu-${QEMU_VERSION}/
 
-RUN patch -p1 < qemu-${QEMU_VERSION}.patch
-
-CMD ./configure --target-list="arm-linux-user" --static \
-	&& make -j $(nproc) \
-	&& strip arm-linux-user/qemu-arm \
-	&& mkdir -p /output/qemu-${QEMU_VERSION} \
-	&& cp arm-linux-user/qemu-arm /output/qemu-${QEMU_VERSION}/qemu-arm-static
+CMD ./build.sh
--- build.sh
+++ build.sh
@@ -1,4 +1,37 @@
 #!/bin/bash
 
-docker build -t qemu .
-docker run -v `pwd`/bin:/output qemu
+set -o errexit
+
+./configure --target-list="arm-linux-user" --static \
+	&& make -j $(nproc) \
+	&& strip arm-linux-user/qemu-arm \
+	&& mkdir -p qemu-$QEMU_VERSION \
+	&& cp arm-linux-user/qemu-arm qemu-$QEMU_VERSION/qemu-arm-static
+
+tar -cvzf qemu-$QEMU_VERSION.tar.gz qemu-$QEMU_VERSION
+SHA256=$(sha256sum qemu-$QEMU_VERSION.tar.gz)
+echo $SHA256
+if [ -z "$ACCOUNT" ] || [ -z "$REPO" ] || [ -z "$ACCESS_TOKEN" ]; then
+	echo "Please set value for ACCOUNT, REPO and ACCESS_TOKEN!"
+	exit 1
+fi
+
+# Create a release
+rm -f request.json response.json
+cat > request.json <<-EOF
+{
+    "tag_name": "$QEMU_VERSION",
+    "target_commitish": "$sourceBranch",
+    "name": "v$QEMU_VERSION",
+    "body": "Release of version $QEMU_VERSION.\nSHA256: ${SHA256% *}",
+    "draft": false,
+    "prerelease": false
+}
+EOF
+curl --data "@request.json" --header "Content-Type:application/json" https://api.github.com/repos/$ACCOUNT/$REPO/releases?access_token=$ACCESS_TOKEN -o response.json
+# Parse response
+RELEASE_ID=$(cat response.json | ./jq '.id')
+echo "RELEASE_ID=$RELEASE_ID"
+
+# Upload data
+curl -H "Authorization:token $ACCESS_TOKEN" -H "Content-Type:application/x-gzip" --data-binary "@qemu-$QEMU_VERSION.tar.gz" https://uploads.github.com/repos/$ACCOUNT/$REPO/releases/$RELEASE_ID/assets?name=qemu-$QEMU_VERSION.tar.gz
